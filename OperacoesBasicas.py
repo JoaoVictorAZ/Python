7cca0e8520dd36b7f6949e7e9267871c02876b7a
João Victor Azevedo de Mendonça <93941432+JoaoVictorAZ@users.noreply.github.com>
2022-01-03 12:13:01 -0300

    Create OperacoesBasicas.py

---
+++ OperacoesBasicas.py
@@ -0,0 +1,8 @@
+num=int(input("Informe um numero:"))
+dobro=num*2
+triplo=num*3
+raiz=num**(1/2)
+raizcubo= num **(1/3)
+pot= num**2
+potcubo= num**3
+print("Para o numero {}:\n Temos que:\n Dobro é {}\n Triplo é {}\n Raiz quadrada é {}\n Raiz cubica é {}\n Elevado ao quadrado é {}\n Elevado ao cubo é {}".format(num,dobro,triplo,raiz,raizcubo,pot,potcubo))
