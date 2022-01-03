704f5bd639da85bc11b6a89325318950a952348d
João Victor Azevedo de Mendonça <93941432+JoaoVictorAZ@users.noreply.github.com>
2022-01-03 12:34:56 -0300

    Create Tabela ConversaoMetro.py

---
+++ Tabela ConversaoMetro.py
@@ -0,0 +1,8 @@
+metro = float(input("Informe um valor em Metros(m):"))
+kilo = metro / 1000
+hecto = metro / 100
+deca = metro / 10
+deci = metro * 10
+cent = metro * 100
+mili = metro * 1000
+print("Para o valor em metros {} temos a tabela:\n Kilometro: {}\n Hectometro: {}\n Decametro {}\n Metro {}\n Decimetro {}\n Centimetro {}\n Milimetro {}".format(metro, kilo, hecto, deca, metro, deci, cent, mili))
