8c751929d15e314c537c306ad10523c3fb253b8c
João Victor Azevedo de Mendonça <93941432+JoaoVictorAZ@users.noreply.github.com>
2022-01-03 13:29:11 -0300

    Create ConversaoCelsius.py

---
+++ ConversaoCelsius.py
@@ -0,0 +1,4 @@
+celsius=float(input("Digite a temperatura em celsius (Cº):"))
+fahr=celsius*1.8+32
+kelvin=celsius+273.15
+print(f"Para o valor em Celsius(Cº) {celsius} temos em conversão:\nFahrenheit: {fahr}\nKelvin: {kelvin}")
