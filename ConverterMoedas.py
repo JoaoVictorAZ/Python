4c000bbf00da4f879e21a1b08fb3e2f60470dc7f
João Victor Azevedo de Mendonça <93941432+JoaoVictorAZ@users.noreply.github.com>
2022-01-03 13:00:41 -0300

    Create ConverterMoedas.py

---
+++ ConverterMoedas.py
@@ -0,0 +1,5 @@
+#exercicio feito no dia 03/01/2022#
+real=float(input("Informe quantos Reais (R$) você quer converter:"))
+dolar= real/5.66
+euro= real/6.39
+print(f"No dia em que este programa foi feito R${real} convertido dava:\n U${dolar} Dolares\n €{euro} Euros")
