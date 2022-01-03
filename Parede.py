e9d82984084400c4b60a99e4ee0d5e1eed82bdca
João Victor Azevedo de Mendonça <93941432+JoaoVictorAZ@users.noreply.github.com>
2022-01-03 13:08:08 -0300

    Create Parede.py

---
+++ Parede.py
@@ -0,0 +1,5 @@
+largura=float(input("Informe a largura da parede:"))
+altura=float(input("Informe agora a altura da parede:"))
+area=largura*altura
+tinta=area/2
+print(f"Para pintar uma parede com {area}M² é preciso comprar {tinta} Litros de tinta")
