2ab52510cb33a95e826e7f6dcee3b8f32e172d6f
João Victor Azevedo de Mendonça <93941432+JoaoVictorAZ@users.noreply.github.com>
2022-01-03 12:16:06 -0300

    Create MediaDeDuasNotas.py

---
+++ MediaDeDuasNotas.py
@@ -0,0 +1,4 @@
+nota1=float(input("Informe a primeira nota:"))
+nota2=float(input("Informe agora a segunda nota:"))
+medianota= (nota1+nota2)/2
+print("A média das notas {} e {} é: {}".format(nota1,nota2,medianota))
