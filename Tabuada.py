dcc995d7ec93fe0d3ca5207501fcd702dd033197
João Victor Azevedo de Mendonça <93941432+JoaoVictorAZ@users.noreply.github.com>
2022-01-03 12:48:12 -0300

    Create Tabuada.py

---
+++ Tabuada.py
@@ -0,0 +1,13 @@
+num=int(input("Informe um número para mostrar a tabuada de de 0 até 10:"))
+zero= num*0
+um= num*1
+dois= num*2
+tres= num*3
+quatro= num*4
+cinco= num*5
+seis= num*6
+sete= num*7
+oito= num*8
+nove= num*9
+dez= num*10
+print(f"A tabuada do número {num} é:\n {num}x0={zero}\n {num}x1={um}\n {num}x2={dois}\n {num}x3={tres}\n {num}x4={quatro}\n {num}x5={cinco}\n {num}x6={seis}\n {num}x7={sete}\n {num}x8={oito}\n {num}x9={nove}\n {num}x10={dez}\n")
