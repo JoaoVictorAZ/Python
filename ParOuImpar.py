c4bc83115cf246324db040679f609aa5b6ecead0
João Victor Azevedo de Mendonça <93941432+JoaoVictorAZ@users.noreply.github.com>
2022-01-07 09:40:21 -0300

    Create ParOuImpar.py

---
+++ ParOuImpar.py
@@ -0,0 +1,7 @@
+num=int(input("Escolha um número e veremos se ele é impar ou par para você:"))
+if num%2==0:
+    print(f"\nVocê escolheu o número {num}")
+    print("O número escolhido é Par")
+else:
+    print(f"\nVocê escolheu o número {num}")
+    print("O número escolhido é Impar")
