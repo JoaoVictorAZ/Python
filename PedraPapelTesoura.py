f292ce938afed5deb36fb1edde57c320bfaa45ed
João Victor Azevedo de Mendonça <93941432+JoaoVictorAZ@users.noreply.github.com>
2022-01-11 09:55:18 -0300

    Update PedraPapelTesoura.py

--- PedraPapelTesoura.py
+++ PedraPapelTesoura.py
@@ -5,7 +5,7 @@ tesoura="3"
 pontoc=0
 pontoh=0
 
-print("\033[1;31mSeja bem vindo ao Jokenpo (Pedra, Papel ou Tesoura)\033[m\n")
+print("\n\033[1;31mSeja bem vindo ao Jokenpo (Pedra, Papel ou Tesoura)\033[m")
 rodadas=int(input("Escolha quantas rodadas você quer que o jogo tenha: "))
 for x in range(rodadas):
     print(f"\033[1;31mBem Vindo a Rodada {x+1}\033[m\n")
@@ -17,43 +17,57 @@ for x in range(rodadas):
 
     if escolha>0 and escolha<4:
         computador = randrange(1, 3)
-        print(f"""\033[1;32mVocê escolheu\033[m {escolha}.
-\033[1;34mO computador escolheu\033[m {computador}.
+        print(f"""\033[1;32mVocê escolheu\033[m {escolha}
+\033[1;34mO computador escolheu\033[m {computador}
 """)
 
         if computador == escolha:
-            print("Uau, Você não perdeu... Mas também não ganhou, isso foi um empate.\n---------------------------------")
-            pontoc + 1 and pontoh + 1
+            print("Uau, Você não perdeu... Mas também não ganhou, isso foi um empate.\n")
+            pontoc +=1
+            pontoh +=1
+            print(f"Seus pontos:{pontoh}\nPontos da máquina: {pontoc}\n---------------------------------")
         ##
         elif computador == 1 and escolha == 2:
-            print("Parabéns, escreveu sua vitória com esse papel hein?\n---------------------------------")
-            pontoh + 1
+            print("Parabéns, escreveu sua vitória com esse papel hein?\n")
+            pontoh +=1
+            print(f"Seus pontos:{pontoh}\nPontos da máquina: {pontoc}\n---------------------------------")
         elif computador == 1 and escolha == 3:
-            print("Ixe maria, Você foi QUEBRADO pela máquina\n---------------------------------")
-            pontoc + 1
+            print("Ixe maria, Você foi QUEBRADO pela máquina\n")
+            pontoc +=1
+            print(f"Seus pontos:{pontoh}\nPontos da máquina: {pontoc}\n---------------------------------")
         ##
         elif computador == 2 and escolha == 1:
-            print("Você foi embrulhado pelo papel digital\n---------------------------------")
-            pontoc + 1
+            print("Você foi embrulhado pelo papel digital\n")
+            pontoc +=1
+            print(f"Seus pontos:{pontoh}\nPontos da máquina: {pontoc}\n---------------------------------")
         elif computador == 2 and escolha == 3:
-            print("Tsc Tsc Tsc, sua tesoura passou pelo papel sem dificuldades\n---------------------------------")
-            pontoh + 1
+            print("Tsc Tsc Tsc, sua tesoura passou pelo papel sem dificuldades\n")
+            pontoh +=1
+            print(f"Seus pontos:{pontoh}\nPontos da máquina: {pontoc}\n---------------------------------")
         ##
         elif computador == 3 and escolha == 1:
-            print("Sua pedra ESBUGALHOU a tesoura inimiga\n---------------------------------")
-            pontoh + 1
+            print("Sua pedra ESBUGALHOU a tesoura inimiga\n")
+            pontoh +=1
+            print(f"Seus pontos:{pontoh}\nPontos da máquina: {pontoc}\n---------------------------------")
         elif computador == 3 and escolha == 2:
-            print("A tesoura digital te rasgou papelzinho ;-;\n---------------------------------")
-            pontoc + 1
+            print("A tesoura digital te rasgou papelzinho ;-;\n")
+            pontoc +=1
+            print(f"Seus pontos:{pontoh}\nPontos da máquina: {pontoc}\n---------------------------------")
     else:
         print("Você não escolheu um número válido")
     if x+1==rodadas:
         if pontoc > pontoh:
-            print("""Resultado:
-            A Skynet te venceu dessa vez, mas quem sabe na próxima...""")
+            print(f"""\033[1;31mResultado:\033[m 
+Seus pontos:{pontoh}
+Pontos da máquina: {pontoc}
+A Skynet te venceu dessa vez, mas quem sabe na próxima...""")
         elif pontoc == pontoh:
-            print("""Resultado:
-            Wow calma lá amigão, você empatou com a máquina? você sabe que isso é raro né?""")
+            print(f"""\033[1;31mResultado:\033[m
+Seus pontos:{pontoh}
+Pontos da máquina: {pontoc}
+Wow calma lá amigão, você empatou com a máquina? você sabe que isso é raro né?""")
         elif pontoc < pontoh:
-            print("""Resultado:
-            TEEEEEMOS UM NOVO CAMPEÃO DE JOKENPO, PARABENS VOCÊ VENCEU A MÁQUINA""")
+            print(f"""\033[1;31mResultado:\033[m
+Seus pontos:{pontoh}
+Pontos da máquina: {pontoc}
+TEEEEEMOS UM NOVO CAMPEÃO DE JOKENPO, PARABENS VOCÊ VENCEU A MÁQUINA""")
